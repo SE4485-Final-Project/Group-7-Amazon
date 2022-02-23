727612a6841868d6f6acab7961ee6ca9319ec602
Omer Reshef <omer1997@gmail.com>
2022-02-22 23:43:10 -0600

    Changed to DynamoDB, Added functions, Changed AWS values to be more secured

---
+++ DynamoDB.py
@@ -0,0 +1,116 @@
+from os import remove
+from pprint import pprint
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+data_j = open("local.json")
+data_o = json.load(data_j)
+
+client = boto3.client('dynamodb',aws_access_key_id=data_o["_ACCESSKEY"], aws_secret_access_key=data_o["_SECRETKEY"],region_name = "us-east-1")
+
+
+#GET an ITEM from specific table using ID.
+def get_item( _tableName,_id):
+    try:
+        response = client.get_item(
+            TableName=_tableName,
+            Key={
+                'ID': {
+                'S': _id
+                }
+            }
+            # For conditions
+            # ConditionExpression="size(info.actors) > :num",
+            # ExpressionAttributeValues={':num': actor_count},
+
+        )
+
+    
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        if ("Item" in response):
+            return response['Item']
+        else :
+            return "Key not found"
+
+#ADD an ITEM to specific table using ID, TYPE, LINK
+def add_item(_tableName,_id,_type,_link):
+    response = client.put_item(
+        TableName = _tableName,
+        Item ={
+            'ID': {
+                'S': _id
+            },
+            'S_Type': {
+                'S': _type
+            },
+            'Link':{
+                'S':  _link
+            }
+        }
+    )
+    return response
+
+#UPDATE an ITEM from a specifc table using ID, TYPE, LINK
+def update_item(_tableName, _id, _type, _link):
+
+    try:
+        response = client.update_item(
+            TableName = _tableName,
+            Key={
+                'ID': {
+                    'S': _id
+                }
+            },
+
+            UpdateExpression="set S_Type=:t, Link=:l",
+            ConditionExpression="ID = :id",
+            ExpressionAttributeValues={
+                ':t': {'S': _type},
+                ':l': {'S': _link},
+                ':id':{'S': _id}
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+            print(e.response['Error']['Message'])
+        else:
+            raise
+    else:
+        return response
+
+#REMOVE an ITEM from a specific table using ID
+def remove_item(_tableName, _id):
+    try:
+        response = client.delete_item(
+            TableName = _tableName,
+            Key={
+                'ID': {
+                    'S': _id
+                }
+            }
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+            print(e.response['Error']['Message'])
+        else:
+            raise
+    else:
+        return response
+
+#PRINTING a given table
+def print_table(_tableName):
+    items = client.scan(
+        TableName = _tableName
+    )["Items"]
+
+    for i in items:
+        print(i)
+
+if __name__ == '__main__':
+
+    print_table("demo")
+    
---
+++ Legacy Files/DB_Handler.py
@@ -0,0 +1,11 @@
+# from pip import main
+# import pymysql
+
+# connection = pymysql.connect(host=_HOST,user=_USERNAME,password=_PASSWORD,database=_DATABASE);
+# with connection:
+#     with connection.cursor() as cursor:
+#         sql = "SELECT * from info"
+#         cursor.execute(sql)
+#         result = cursor.fetchall()
+#         for t in result:
+#             print(t)
